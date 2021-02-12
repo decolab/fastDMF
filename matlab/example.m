100bd64eda125bdec43269306d54360002e3a775
Pedro Mediano <pedro.martinez-mediano13@imperial.ac.uk>
2021-02-12 09:29:34 +0000

    Added BOLD post-processing code to Python and Matlab examples.

--- matlab/example.m
+++ matlab/example.m
@@ -8,7 +8,15 @@
 % Fetch default parameters
 params = DefaultParams();
 
-% Run simulation
-nb_steps = 20000;
-b = DMF(params, nb_steps);
+% Run simulation for a given nb of steps (milliseconds)
+nb_steps = 100000;
+BOLD = DMF(params, nb_steps);
+
+% Minimal "post-processing": band-pass filter and remove the starting and
+% trailing ends of the simulation to avoid transient and filtering artefacts
+[B, A] = butter(2, [0.01, 0.1]*2*params.TR);
+BOLD = filter(B, A, BOLD')';
+
+trans = 5;
+BOLD = BOLD(:, 1+trans:end-trans);
 
